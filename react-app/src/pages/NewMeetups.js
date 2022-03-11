595b922def51a1c79d967fb8f8e217286c20ee3c
Evka K <kuklaewa@gmail.com>
2022-03-11 14:18:41 +0000

    add React course exercise

---
+++ react-app/src/pages/NewMeetups.js
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  const navigate = useNavigate();
+  
+  function addMeetupHandler(meetupData) {
+    fetch('https://virtual-kiln-323110-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      navigate('/', { replace: true });
+    });
+  }
+  return (
+    <section>
+      <h1>New Meetup Page</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
